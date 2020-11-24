683eb668ded5188159a7ddfa4733ccc0d7f41b13
kurikuto <k.ab.kurikuto@gmail.com>
2020-11-24 21:37:13 +0900

    フォームの作成

--- web/src/main/java/com/mizuumi/words/web/HomeController.java
+++ web/src/main/java/com/mizuumi/words/web/HomeController.java
@@ -1,14 +1,30 @@
 package com.mizuumi.words.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/home")
+    public ModelAndView get(ModelAndView mav) {
+        mav.addObject("userId", "dummy");
+        mav.setViewName("home");
+        return mav;
+    }
     
-    @RequestMapping("/home")
-    public String get() {
-        return "home";
+    @PostMapping("/home")
+    public ModelAndView post(
+            @RequestParam("id")String userId, 
+            @RequestParam("password")String password,
+            ModelAndView mav) 
+    {
+        mav.addObject("userId", userId);
+        mav.setViewName("home");
+        return mav;
     }
 
 }
--- web/src/main/java/com/mizuumi/words/web/WordsController.java
+++ web/src/main/java/com/mizuumi/words/web/WordsController.java
@@ -2,13 +2,21 @@ package com.mizuumi.words.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WordsController {
     
     @RequestMapping("/words/read")
-    public String read() {
-        return "read";
+    public ModelAndView read(ModelAndView mav) {
+        int num = 10;
+        int res = 0;
+        for (int i = 1; i < num; i++) {
+            res += 1;
+        }
+        mav.addObject("source", "出典" + res);
+        mav.setViewName("read");
+        return mav;
     }
 
     @RequestMapping("/words/register")
