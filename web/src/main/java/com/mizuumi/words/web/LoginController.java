face5efebbc1b33eee18eede125d244805f8f6fc
kurikuto <k.ab.kurikuto@gmail.com>
2020-11-25 21:11:00 +0900

    ログイン仮判定など追加

--- web/src/main/java/com/mizuumi/words/web/LoginController.java
+++ web/src/main/java/com/mizuumi/words/web/LoginController.java
@@ -1,14 +1,31 @@
 package com.mizuumi.words.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
     
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String get() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView post(
+        @RequestParam("id")String userId, 
+        @RequestParam("password")String password,
+        ModelAndView mav
+    ) {
+        if(password.equals("pass123")) {
+            return new ModelAndView("redirect:/home");
+        }
+        mav.addObject("msg", "パスワードが誤っています。");
+        mav.setViewName("login");
+        return mav;
     }
 
 }
--- web/src/main/java/com/mizuumi/words/web/WordsListController.java
+++ web/src/main/java/com/mizuumi/words/web/WordsListController.java
@@ -1,14 +1,27 @@
 package com.mizuumi.words.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WordsListController {
     
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String get() {
         return "list";
+    }
+
+    @PostMapping("/list")
+    public ModelAndView post(
+        @RequestParam(value="category",required=false)String category,
+        ModelAndView mav) 
+    {
+        mav.addObject("category", "category: " + category);
+        mav.setViewName("list");
+        return mav;
     }
 
 }
