57cc3a79e2a790b64041d245a1cc578c776acb76
kurikuto <k.ab.kurikuto@gmail.com>
2021-01-09 14:58:46 +0900

    validation追加

--- web/src/main/java/com/mizuumi/words/web/controller/WordsRegisterController.java
+++ web/src/main/java/com/mizuumi/words/web/controller/WordsRegisterController.java
@@ -10,6 +10,8 @@ import com.mizuumi.words.web.util.dateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,22 +52,31 @@ public class WordsRegisterController {
     @PostMapping("/words/register")
     @Transactional
     public ModelAndView register(
-        @ModelAttribute("formModel") WordsDto dto,
+        @ModelAttribute("formModel") 
+        @Validated WordsDto dto,
+        BindingResult result,
         ModelAndView mav
     ) {
-        int memberId = 1001; // 暫定
+        if(!result.hasErrors()) {
+            int memberId = 1001; // 暫定
 
-        // コントローラで暫定処理
-        WordsEntity entity = new WordsEntity();
-        BeanUtils.copyProperties(dto, entity);
-        //日付
-        entity.setCreated(dateUtil.getNowDate());
-        entity.setModified(dateUtil.getNowDate());
-        entity.setDelFlg(false);
-        entity.setMemberId(memberId);
-        wordsRepository.saveAndFlush(entity);
+            // コントローラで暫定処理
+            WordsEntity entity = new WordsEntity();
+            BeanUtils.copyProperties(dto, entity);
+            //日付
+            entity.setCreated(dateUtil.getNowDate());
+            entity.setModified(dateUtil.getNowDate());
+            entity.setDelFlg(false);
+            entity.setMemberId(memberId);
+            wordsRepository.saveAndFlush(entity);
+    
+            return new ModelAndView("redirect:/home");          
+        } else {
+            mav.addObject("error", true);
+        }
 
-        return new ModelAndView("redirect:/home");
+        mav.setViewName("register");
+        return mav;
     }
 
 }
--- web/src/main/java/com/mizuumi/words/web/dto/WordsDto.java
+++ web/src/main/java/com/mizuumi/words/web/dto/WordsDto.java
@@ -1,18 +1,32 @@
 package com.mizuumi.words.web.dto;
 
 import lombok.Data;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class WordsDto {
+
     private long wordsId;
-    private int memberId;
+
+    private Integer memberId;
+
     private String title;
+
+    @NotEmpty
     private String words;
+
+    @NotEmpty
     private String source;
-    private int category;
+
+    private Integer category;
+
+    @NotEmpty
     private String wordsDate;
+
     private String created;
+
     private String contributor;
-    private boolean openFlg;
+
+    private Boolean openFlg;
 
 }
